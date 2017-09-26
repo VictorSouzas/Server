4cd2f026745e5e0e1f752bf45aa671317ffd8b14
Victor Souza <victor_souzas@yahoo.com.br>
2017-09-26 17:25:29 -0300

    Server file

---
+++ Server.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using BinaryTree;
+
+namespace Server
+{
+    public class Server 
+    {
+        private IPEndPoint EndPoint;
+        private Socket ServerSocket;
+        private BinaryTreeThread binaryTree;
+        public Server()
+        {
+            ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream,ProtocolType.Tcp);
+            EndPoint = new IPEndPoint(IPAddress.Any, 8888);
+            ServerSocket.Bind(EndPoint);
+            ServerSocket.Listen(backlog:Int32.MaxValue);
+            binaryTree = new BinaryTreeThread();
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("waiting");
+            Socket client = ServerSocket.Accept();
+            Console.WriteLine("----------------connected------------------------");
+            StreamClient stream = new StreamClient(client);
+            Thread t = new Thread(stream.service);
+            t.Start();
+            binaryTree.Add(t);  
+        }
+        
+        
+    }
+}
---
+++ StreamClient.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    public class StreamClient
+    {
+        private Socket Client;
+        public StreamClient(Socket client)
+        {
+            Client = client;
+        }
+
+        public void service()
+        {
+            try
+            {
+                while (true)
+                {
+                    byte[] buffer = new byte[64];
+                    Waiting(buffer);
+                    Answering(buffer);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Connection Terrminated");
+            }
+        }
+
+        private void Waiting(byte[] buffer)
+        {
+            
+            Console.WriteLine("Waiting for an answer");
+            Client.Receive(buffer, 0, buffer.Length, 0);
+            string message = Encoding.UTF8.GetString(buffer);
+            Console.WriteLine("Answer: " + message);
+        }
+
+        private void Answering(byte[] buffer)
+        {
+            string answer = "Actualy date is " + DateTime.Now;
+            buffer = Encoding.UTF8.GetBytes(answer);
+            Console.WriteLine("Sending {0}", answer);
+            Client.Send(buffer);
+        }
+        
+        
+        
+    }
+}
